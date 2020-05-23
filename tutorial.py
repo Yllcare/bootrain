64373b14607b9a75f2540cf5c782f286b79e4192
Yllcare <39796100+Yllcare@users.noreply.github.com>
2020-05-23 19:43:17 +0300

    this is the first assignment of bootrain.com

---
+++ tutorial.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 23 18:50:49 2020
+
+@author: illcare
+"""
+
+print("illcare sonear")
+print('''illcare \n sonear''')
+print('''`I don't want to be an "artist". I want to be a "Data Scientist."`''')
+      
