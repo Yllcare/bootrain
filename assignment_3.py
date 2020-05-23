14cd8ebda3c7a436973727f7d7c8944db1b2652e
Yllcare <39796100+Yllcare@users.noreply.github.com>
2020-05-23 23:33:41 +0300

    this is the 3rd assignment of bootrain.com

---
+++ assignment_3.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 23 23:24:09 2020
+
+@author: illcare
+"""
+
+x = y = 1
+print("value and type of variable x :", x, type(x))
+print("value and type of variable y :", y, type(y))
+
+y = str(y)
+print("type of variable y is changed into", type(y))
+# what do you mean by "make the two variables the same"
+x = y # maybe you mean this ?!
