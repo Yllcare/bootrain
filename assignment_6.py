b1a9be09eaf7671c4031081a0e3beeaec85e3b8b
Yllcare <39796100+Yllcare@users.noreply.github.com>
2020-05-24 15:43:59 +0300

    this is the 6th assignment of bootrain.com

---
+++ assignment_6.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 24 13:15:07 2020
+
+@author: illcare
+"""
+
+week = ("monday","tuesday","wednesday","thursday","friday","saturday","sunday") # 1
+fruits = {"apple","mango","orange"} # 2
+new_fruits = {"cherry","peach","apple","mango"} # 3
+print(new_fruits.difference(fruits)) # 4
+print(new_fruits.intersection(fruits)) # 5
