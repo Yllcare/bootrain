d93fdd67970ddf5ae71d4731703178e18b6b2458
Yllcare <39796100+Yllcare@users.noreply.github.com>
2021-01-23 17:01:40 +0300

    Update assignment_11.py

--- assignment_11.py
+++ assignment_11.py
@@ -9,23 +9,33 @@ Created on Wed Jun 24 01:26:06 2020
 def div(dvdnd,dvsr):
     try:
         res = dvdnd/dvsr
+        print("\ndivision result is : ",res,sep="")
         return res
     except ZeroDivisionError:
-        print("Divisor can not be zero")
-    except NameError:
-        print("Global or not, whatever name is absent error")
+        print("\nDivisor can not be zero")
         
 d = div(20,0)
-print(d)
 
 # 2
+def div(dvdnd,dvsr):
+    try:
+        res = dvdnd/dvsr
+        prit("\ndivision result is : ",res,sep="")
+        return res
+    except NameError:
+        print("\nGlobal or not, whatever. It is a nameError")
+        
+d = div(20,2)
+
+# 3
 def sqr(no):
     try:
         n = int(no)
+        print("square of the number you entered is", n**2)
         return n**2
     except ValueError:
         print("enter a valid value, meaning a number")
         
 
 nono = input("insert a number :")
-print("square of the number you entered is", sqr(nono))
+result = sqr(nono)
